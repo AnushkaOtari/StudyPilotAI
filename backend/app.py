47ba6bc41817bd0a82654583e3559e009587e383
Anushka Otari <anuotari27@gmail.com>
2026-06-12 17:03:13 +0530

    Built first working Basic StudyPilot AI web app

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from models import QuestionRequest
 from rag.rag_pipeline import ask_rag
 
 app = FastAPI()
+
+# Allow React frontend to talk to FastAPI
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
@@ -12,10 +23,10 @@ def root():
 
 @app.post("/ask")
 def ask_question(data: QuestionRequest):
-
     answer = ask_rag(
-        data.question
+    data.question
     )
+
 
     return {
         "question": data.question,
